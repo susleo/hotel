703e6a1e521306cdcd0c9bd82e3392b117f2d798
Suraj <surajfromleo@gmail.com>
2020-05-06 11:13:00 +0545

    Laravel intiliazed

---
+++ database/migrations/2020_05_06_051516_create_guests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('image')->nullable();
+            $table->string('city')->nullable();
+            $table->string('area');
+            $table->string('country');
+            $table->string('number');
+            $table->string('email');
+            $table->string('gender')->nullable();
+            $table->string('status')->default('0');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
